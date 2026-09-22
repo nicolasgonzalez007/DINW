f98ccc4be101610098bc9ff2aede5e84d8c9176f
Nicolás Manuel <ngonzalezmorera.guadalupe@alumnado.fundacionloyola.net>
2026-09-22 17:38:28 +0200

    Primer commit

---
+++ datos.php
@@ -0,0 +1,12 @@
+<?php
+    $asignatura=$_GET['asignatura'];
+    $profesores=$_GET['profesor'];
+    $horas=$_GET['horas'];
+    $info=$_GET['informacion'];
+    echo "Asignatura: ".$asignatura."<br>";
+    echo "Profesores: ".$profesores."<br>";
+    echo "Horas: ".$horas."<br>";
+    echo "Información: ".$info."<br>";
+    
+
+?>
